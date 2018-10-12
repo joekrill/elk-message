4be49bf8358b11eeffe18047e902bfa51e2bd107
Joe Krill <joe@joekrill.com>
2018-10-12 08:23:46 -0400

    fix: include reserved characters when parsing areas
    Previously the AlarmMemoryUpdate was only returning data for areas 1-6.

--- src/messages/responses/AlarmMemoryUpdate.ts
+++ src/messages/responses/AlarmMemoryUpdate.ts
@@ -1,18 +1,37 @@
 import ElkResponse from './ElkResponse';
 
 /**
- * 4.38 Alarm Memory Update (AM)
+ * This message is used to inform User Interfaces that an alarm
+ * memory is present and must be cleared before an arm command will arm the system.
+ *
+ * @see 4.38 Alarm Memory Update (AM)
  */
 export default class AlarmMemoryUpdate extends ElkResponse {
   static readonly COMMAND = 'AM';
 
+  /**
+   * An array containing the alarm memory status of each area.
+   * If the value is true, there is an alarm memory present for
+   * the area. Index 0 indicates area 1's alarm memory status,
+   * index 1 indicates area 2's alarm memory status, etc.
+   */
   readonly areas: boolean[];
 
   constructor(raw: string) {
     super(raw);
-    this.areas = Array.from(this.data.substr(0, 8)).map(char => char === '1');
+
+    // The alarm memory response uses the reserved characters as
+    // part of it's data (or depending on how you look at it,
+    // doesn't *have* the reserved characters.
+    const data = this.data + this.reserved;
+    this.areas = Array.from(data.substr(0, 8)).map(char => char === '1');
   }
 
+  /**
+   * Returns a value indicating whether there is an alarm memory
+   * present in the given area.
+   * @param areaNumber The area number (1..8)
+   */
   isAlarmMemoryPresent(areaNumber: number) {
     return this.areas[areaNumber - 1];
   }
